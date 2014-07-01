a58b330967a56dbd6c5150309e42be3374d163d8
hombre7lobo <r.scherbarth@gmail.com>
2014-07-02 00:13:24 +0200

    name added

--- DistClient/DistClient/main.m
+++ DistClient/DistClient/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  DistClient
 //
-//  Created by elwoodb on 29.06.14.
+//  Created by Robert Scherbarth on 29.06.14.
 //  Copyright (c) 2014 Robert Scherbarth. All rights reserved.
 //
 
